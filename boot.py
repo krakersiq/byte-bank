6983000d391ec114718f58535c8962c115ce869e
Antonia Rybaczyk <anrybaczyk@gmail.com>
2026-08-10 12:14:15 +0200

    Add WiFi connection logic in boot.py

---
+++ boot.py
@@ -0,0 +1,12 @@
+import network
+
+
+print("Connecting to WiFi", end="")
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect('Rechnernetze', 'rnFIW625')
+while not sta_if.isconnected():
+  print(".", end="")
+  time.sleep(0.1)
+print(" Connected!")
+
