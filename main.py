af936df2504e90a4ac3aee1f5cd4936a9a6e7d51
Antonia Rybaczyk <anrybaczyk@gmail.com>
2026-08-11 09:46:58 +0200

    Update main.py
    Removed introductory comments and copyright notice.

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-"""
-MicroPython IoT Weather Station Example for Wokwi.com
-
-To view the data:
-
-1. Go to http://www.hivemq.com/demos/websocket-client/
-2. Click "Connect"
-3. Under Subscriptions, click "Add New Topic Subscription"
-4. In the Topic field, type "wokwi-weather" then click "Subscribe"
-
-Now click on the DHT22 sensor in the simulation,
-change the temperature/humidity, and you should see
-the message appear on the MQTT Broker, in the "Messages" pane.
-
-Copyright (C) 2022, Uri Shaked
-
-https://wokwi.com/arduino/projects/322577683855704658
-"""
-
 import time
 from machine import Pin
 import dht
