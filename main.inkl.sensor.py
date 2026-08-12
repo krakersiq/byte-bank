b5a99fc9266710451847226966792f0c97a7fbd3
nellipequena <48388603+nellipequena@users.noreply.github.com>
2026-08-12 14:49:01 +0200

    Update main.inkl.sensor.3py

--- main.inkl.sensor.py
+++ main.inkl.sensor.py
@@ -10,8 +10,14 @@ led_rot = Pin(14, Pin.OUT)     # D14 für Rot
 led_gruen = Pin(18, Pin.OUT)   # D12 für Grün
 sensor = dht.DHT22(Pin(4))     # D4 für den DHT22 Temperatur-Sensor
 
-sensor_pin = ADC(Pin(32))       #MS
-sensor_pin.atten(ADC.ATTN_11DB) #MS
+#Feuchtigkeitssensor (MS)
+sensor_pin = ADC(Pin(32))       
+sensor_pin.atten(ADC.ATTN_11DB)
+
+# Wasserqualität TDS
+water_pin = ADC(Pin(34))
+water_pin.atten(ADC.ATTN_11DB)
+
 
 # MQTT Server Parameters
 MQTT_CLIENT_ID = "micropython-weather-demo"
@@ -28,6 +34,7 @@ print("Connected!")
 prev_temp = None
 prev_humidity = None
 prev_moisture = None  #MS
+prev_water = None    #TDS
 
 while True:
     print("Measuring weather conditions... ", end="")
@@ -36,6 +43,7 @@ while True:
     temp = sensor.temperature()
     humidity = sensor.humidity()
     moisture = sensor_pin.read() # je kleiner der Wert, desto feuchter der Boden , MS
+    quality = water_pin.read() #WQ
 
     # --- 2. LAMPEN STEUERN (> 30 Grad = Rot, sonst Grün) ---
     if temp > 30:
@@ -46,20 +54,26 @@ while True:
         led_gruen.value(1)    # Grün AN
         
    # --- VERGLEICH VOR TRANSFORMATION ---
-    if temp != prev_temp or humidity != prev_humidity:
+    if (temp != prev_temp or humidity != prev_humidity or moisture != prev_moisture
+        or quality != prev_water):
         print("Updated!")
         message = ujson.dumps({
             "temp": temp,
             "humidity": humidity,
             "moisture": moisture, #MS
+            "quality": quality
         })
+        
         print("Reporting to MQTT topic {}: {}".format(MQTT_TOPIC, message))
         client.publish(MQTT_TOPIC, message)
         prev_temp = temp
         prev_humidity = humidity
         prev_moisture = moisture #MS
+        prev_water = quality
     else:
         print("No change")
 
     time.sleep(1)
+   
+
    
