117ec9eab7ef4040cf0af2cbc1f17cc8cc66585d
nellipequena <48388603+nellipequena@users.noreply.github.com>
2026-08-12 13:50:32 +0200

    Update main.inkl.sensor.py
    Feuchtigkeitssensor vollständig implementiert

--- main.inkl.sensor.py
+++ main.inkl.sensor.py
@@ -1,6 +1,6 @@
 import time
 from machine import Pin
-from machine import Pin, ADC #Moisture Sensor
+from machine import Pin, ADC  #Moisture Sensor(MS)
 import dht
 import ujson
 from umqtt.simple import MQTTClient
@@ -9,6 +9,9 @@ from umqtt.simple import MQTTClient
 led_rot = Pin(14, Pin.OUT)     # D14 für Rot
 led_gruen = Pin(18, Pin.OUT)   # D12 für Grün
 sensor = dht.DHT22(Pin(4))     # D4 für den DHT22 Temperatur-Sensor
+
+sensor_pin = ADC(Pin(32))       #MS
+sensor_pin.atten(ADC.ATTN_11DB) #MS
 
 # MQTT Server Parameters
 MQTT_CLIENT_ID = "micropython-weather-demo"
@@ -22,7 +25,9 @@ client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, user=MQTT_USER, password=MQTT_P
 client.connect()
 print("Connected!")
 
-prev_weather = ""
+prev_temp = None
+prev_humidity = None
+prev_moisture = None  #MS
 
 while True:
     print("Measuring weather conditions... ", end="")
@@ -30,6 +35,7 @@ while True:
     
     temp = sensor.temperature()
     humidity = sensor.humidity()
+    moisture = sensor_pin.read() # je kleiner der Wert, desto feuchter der Boden , MS
 
     # --- 2. LAMPEN STEUERN (> 30 Grad = Rot, sonst Grün) ---
     if temp > 30:
@@ -38,19 +44,22 @@ while True:
     else:
         led_rot.value(0)      # Rot AUS
         led_gruen.value(1)    # Grün AN
-    
-    message = ujson.dumps({
-        "temp": temp,
-        "humidity": humidity,
-       # "time": time_str
-    })
-
-    if message != prev_weather:
+        
+   # --- VERGLEICH VOR TRANSFORMATION ---
+    if temp != prev_temp or humidity != prev_humidity:
         print("Updated!")
+        message = ujson.dumps({
+            "temp": temp,
+            "humidity": humidity,
+            "moisture": moisture, #MS
+        })
         print("Reporting to MQTT topic {}: {}".format(MQTT_TOPIC, message))
         client.publish(MQTT_TOPIC, message)
-        prev_weather = message
+        prev_temp = temp
+        prev_humidity = humidity
+        prev_moisture = moisture #MS
     else:
         print("No change")
 
     time.sleep(1)
+   
